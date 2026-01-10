097aa618c9af4a61f0e79cfae08f0162b85c2db4
Czarek <czarekmelibruda@op.pl>
2026-01-10 18:48:49 +0100

    initial commit

---
+++ src/test/java/pl/lifty_api/LiftyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lifty_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
