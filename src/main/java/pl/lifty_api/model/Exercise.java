c2a7957a0839a1b01730218090aabf48892e4186
Czarek <czarekmelibruda@op.pl>
2026-01-15 21:04:31 +0100

    initial

---
+++ src/main/java/pl/lifty_api/model/Exercise.java
@@ -0,0 +1,24 @@
+package pl.lifty_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Exercise {
+
+    @Id
+    private UUID id;
+
+    private String name;
+    private String description;
+}
